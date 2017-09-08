64c31606060adbf1212605b2e6752188e02e2df0
c0nn3r <connervercellino@gmail.com>
2017-09-08 01:33:43 -0700

    Moved layers to folder

--- models.py
+++ models.py
@@ -28,28 +28,6 @@ model_urls = {
 }
 
 
-def init_conv_weights(layer):
-    nn.init.normal(layer.weight.data, std=0.01)
-    nn.init.constant(layer.bias.data, val=0)
-    return layer
-
-
-def conv1x1(in_channels, out_channels, **kwargs):
-
-    layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, **kwargs)
-    layer = init_conv_weights(layer)
-
-    return layer
-
-
-def conv3x3(in_channels, out_channels, **kwargs):
-
-    layer = nn.Conv2d(in_channels, out_channels, kernel_size=3, **kwargs)
-    layer = init_conv_weights(layer)
-
-    return layer
-
-
 class BasicBlockFeatures(BasicBlock):
 
     def forward(self, x):
