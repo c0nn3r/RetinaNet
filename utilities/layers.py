18d6c50e17b61198ce0b58a7378107243faed284
c0nn3r <connervercellino@gmail.com>
2017-09-09 14:03:00 -0700

    New file for layer inits

--- utilities/layers.py
+++ utilities/layers.py
@@ -1,13 +1,21 @@
 import torch.nn as nn
 
 
-def init_conv_weights(layer):
-    nn.init.normal(layer.weight.data, std=0.01)
-    nn.init.constant(layer.bias.data, val=0)
+def init_conv_weights(layer, weights_std=0.01,  bias=0):
+    ''''
+    RetinaNet's layer initialization
+
+    :layer
+    :
+
+    ''''
+    nn.init.normal(layer.weight.data, std=weights_std)
+    nn.init.constant(layer.bias.data, val=bias)
     return layer
 
 
 def conv1x1(in_channels, out_channels, **kwargs):
+    '''Return a 1x1 convolutional layer with RetinaNet's weight and bias initialization'''
 
     layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, **kwargs)
     layer = init_conv_weights(layer)
@@ -16,6 +24,7 @@ def conv1x1(in_channels, out_channels, **kwargs):
 
 
 def conv3x3(in_channels, out_channels, **kwargs):
+    '''Return a 3x3 convolutional layer with RetinaNet's weight and bias initialization'''
 
     layer = nn.Conv2d(in_channels, out_channels, kernel_size=3, **kwargs)
     layer = init_conv_weights(layer)
